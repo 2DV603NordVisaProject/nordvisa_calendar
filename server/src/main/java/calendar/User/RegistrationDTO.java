9e52b1ae3717c4ca2aa5204a5edd7b64f8728ac5
Axel Nilsson <an222yp@student.lnu.se>
2017-05-09 00:05:22 +0200

    Added RegistrationDTO and UserUpdateDTO

---
+++ server/src/main/java/calendar/User/RegistrationDTO.java
@@ -0,0 +1,13 @@
+package calendar.User;
+
+/**
+ * Class RegistrationDTO
+ *
+ * @author Axel Nilsson (axnion)
+ */
+class RegistrationDTO {
+    String email;
+    String password;
+    String passwordConfiration;
+    String organization;
+}
---
+++ server/src/main/java/calendar/User/UserUpdateDTO.java
@@ -0,0 +1,14 @@
+package calendar.User;
+
+/**
+ * Class UserUpdateDTO
+ *
+ * @author Axel Nilsson (axnion)
+ */
+class UserUpdateDTO {
+    public String email;
+    public String oldPassword;
+    public String password;
+    public String passwordConfirmation;
+    public String organization;
+}
