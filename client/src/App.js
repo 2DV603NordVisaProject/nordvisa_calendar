f172e9c15f549ac83bb98f11cd6bf0dd9bfeb1c0
johang2012 <johan.gudmundsson2012@gmail.com>
2017-05-10 23:57:15 +0200

    added LoginView component

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import TopBar from "./components/TopBar";
 import ViewContainer from "./components/ViewContainer"
+import Router from "react-router/BrowserRouter";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <TopBar/>
-        <ViewContainer/>
-      </div>
+      <Router>
+        <div className="App">
+          <TopBar/>
+          <ViewContainer/>
+        </div>
+      </Router>
     );
   }
 }
--- client/src/components/ViewContainer.js
+++ client/src/components/ViewContainer.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import "./ViewContainer.css";
+import LoginView from "./LoginView";
+import Match from "react-router/Match";
 
 class ViewContainer extends Component {
   render() {
     return (
       <div className="view-container">
+        <Match pattern="/login" component={LoginView}/>
       </div>
     );
   }
