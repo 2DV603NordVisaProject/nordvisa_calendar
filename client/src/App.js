a640fc0620ac367b97963aa5e9941362eed3cfe1
johang2012 <johan.gudmundsson2012@gmail.com>
2017-05-10 20:23:02 +0200

    added ViewContainer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import TopBar from "./components/TopBar";
+import ViewContainer from "./components/ViewContainer"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopBar/>
+        <ViewContainer/>
       </div>
     );
   }
---
+++ client/src/components/ViewContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./ViewContainer.css";
+
+class ViewContainer extends Component {
+  render() {
+    return <div className="viewContainer">hello world</div>;
+  }
+}
+
+export default ViewContainer;
