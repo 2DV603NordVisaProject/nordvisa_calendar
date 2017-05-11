9786f3c08d0e1445b29fb6c5c9cda92fb686c53f
johang2012 <johan.gudmundsson2012@gmail.com>
2017-05-11 03:27:03 +0200

    Added UpdatePasswordView component

--- client/src/components/ViewContainer.js
+++ client/src/components/ViewContainer.js
@@ -4,6 +4,7 @@ import LoginView from "./LoginView";
 import Match from "react-router/Match";
 import RegisterView from "./RegisterView";
 import RecoverView from "./RecoverView";
+import UpdatePasswordView from "./UpdatePasswordView";
 
 class ViewContainer extends Component {
   render() {
@@ -12,6 +13,7 @@ class ViewContainer extends Component {
         <Match pattern="/login" component={LoginView}/>
         <Match pattern="/register" component={RegisterView}/>
         <Match pattern="/recover-password" component={RecoverView}/>
+        <Match pattern="/update-password" component={UpdatePasswordView}/>
       </div>
     );
   }
