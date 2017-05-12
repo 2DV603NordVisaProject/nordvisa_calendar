5859a10237f7b9bc3623543dca81addf38cfa623
johang2012 <johan.gudmundsson2012@gmail.com>
2017-05-12 22:56:44 +0200

    init states for LoginView component

--- client/src/components/LoginView.js
+++ client/src/components/LoginView.js
@@ -2,16 +2,42 @@ import React, { Component } from "react";
 import "./LoginView.css";
 
 class LoginView extends Component {
+  state = {
+    fields: {
+      email: "",
+      password: "",
+    },
+  }
+
+  onFormSubmit(event) {
+    //TODO
+  }
+
+  onInputChange(event) {
+    let fields = this.state.fields;
+    fields[event.target.name] = event.target.value;
+    this.setState({fields})
+  }
   render() {
     return (
       <div className="lightbox login">
         <h2>Login</h2>
-        <form>
+        <form onSubmit={this.onFormSubmit.bind(this)}>
           <label htmlFor="email">Email:</label>
-          <input name="email" type="text"></input>
+          <input
+            name="email"
+            value={this.state.fields.email}
+            onChange={this.onInputChange.bind(this)}
+            type="text">
+          </input>
           <label htmlFor="password">Password:</label>
-          <input name="password" type="password"></input>
-          <button>login</button>
+          <input
+            name="password"
+            onChange={this.onInputChange.bind(this)}
+            value={this.state.fields.password}
+            type="password">
+          </input>
+          <input type="submit" className="btn-primary" value="login"></input>
         </form>
         <a href="/recover-password">Forgot Password?</a>
       </div>
