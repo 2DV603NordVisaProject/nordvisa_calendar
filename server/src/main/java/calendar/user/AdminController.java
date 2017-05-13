a63f589a06bdfd1adfe5c5e7767287b1b0afc8b4
Axel Nilsson <an222yp@student.lnu.se>
2017-05-13 18:26:30 +0200

    Added comments to AdminController

--- server/src/main/java/calendar/user/AdminController.java
+++ server/src/main/java/calendar/user/AdminController.java
@@ -10,6 +10,15 @@ import java.util.ArrayList;
 /**
  * Class AdminController
  *
+ * This class maps to /api/admin, and contains all available routes for calling the API as an
+ * authenticated administrator.
+ *
+ * The following routes are available:
+ * Make User                    -   POST    -   /api/admin/make_user
+ * Make Administrator           -   POST    -   /api/admin/make_admin
+ * Get pending registrations    -   GET     -   /api/admin/registrations
+ * Accept/Deny Registration     -   POST    -   /api/admin/registrations
+ *
  * @author Axel Nilsson (axnion)
  */
 @RestController
@@ -17,6 +26,13 @@ import java.util.ArrayList;
 public class AdminController {
     private UserDAO dao = new UserDAOMongo();
 
+    /**
+     * This method take a user ID in a UserIdDTO and if conditions are meet then demotes the target
+     * User to role USER
+     *
+     * @param dto           UserIdDTO containing the user ID of the target user
+     * @throws Exception    Unauthorized or database errors
+     */
     // TODO: NOT TESTED!
     @RequestMapping(value = "/make_user", method = RequestMethod.POST)
     public void makeUser(@ModelAttribute UserIdDTO dto) throws Exception {
@@ -36,6 +52,13 @@ public class AdminController {
 
     }
 
+    /**
+     * Takes a user ID in a UserIdDTO and if conditions are meet then promotes the target user to
+     * the role ADMIN
+     *
+     * @param dto           UserIdDTO containing the user ID of the target user
+     * @throws Exception    Unauthorized or database errors
+     */
     // TODO: NOT TESTED!
     @RequestMapping(value = "/make_admin", method = RequestMethod.POST)
     public void makeAdmin(@ModelAttribute UserIdDTO dto) throws Exception {
@@ -50,16 +73,33 @@ public class AdminController {
         }
     }
 
+    /**
+     * Fetches all pending registrations and pending organization registration and returns them
+     * to the client.
+     *
+     * @return              ArrayList if User objects of which are waiting to be approved or denied
+     * @throws Exception    Database errors
+     */
     @RequestMapping(value = "/registrations", method = RequestMethod.GET)
     @ResponseBody
     public ArrayList<User> getPendingRegistrations() throws Exception {
         return dao.getPendingRegistrations();
     }
 
+    /**
+     * Takes the id of the target user and a boolean value if the registration was approved or not
+     * in a RegistrationDecisionDTO. The method checks if it's a registration or orgnization change.
+     * These are treated a bit differently. A registation calls approveRegistration if the
+     * registration was approved, and deletes the user if the registration was denied.
+     *
+     * @param dto           Id and approved boolean in an object
+     * @throws Exception    Database errors.
+     */
     @RequestMapping(value = "/registrations", method = RequestMethod.POST)
     public void registrationDecision(@ModelAttribute RegistrationDecisionDTO dto) throws Exception {
         User user = dao.getUserById(dto.getId());
 
+        // TODO: Double check this logic!
         if(user.getOrganization().getChangePending().equals("")) {
             if (dto.isApproved())
                 dao.approveRegistration(dto.getId());
