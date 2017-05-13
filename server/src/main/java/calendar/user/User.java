d06c425eadb9d75b78d5c09aa59b35257f69519c
Axel Nilsson <an222yp@student.lnu.se>
2017-05-13 17:57:15 +0200

    Added method for checking if action is posible on user

--- server/src/main/java/calendar/user/User.java
+++ server/src/main/java/calendar/user/User.java
@@ -99,18 +99,6 @@ class User {
         return validateEmailLink;
     }
 
-    long getCreatedAt() {
-        return createdAt;
-    }
-
-    long getUpdatedAt() {
-        return updatedAt;
-    }
-
-    ArrayList<String> getEvents() {
-        return events;
-    }
-
     Organization getOrganization() {
         return organization;
     }
@@ -136,20 +124,91 @@ class User {
         this.validateEmailLink = validateEmailLink;
     }
 
-//    void setCreatedAt(long createdAt) {
-//        this.createdAt = createdAt;
-//    }
+    /**
+     * The method determines if this user is authorized to manage the other account. Manage involves
+     * editing of events created by target user, unregister the account.
+     *
+     * @param target    The target user which this user want to do at action on.
+     * @return          True if this user can manage the target user
+     */
+    boolean canManage(User target) {
+        boolean answer = false;
 
-    void setUpdatedAt(long updatedAt) {
-        this.updatedAt = updatedAt;
+        if(id.equals(target.getId())) {
+            answer = true;
+        }
+        else if(role.equals("SUPER_ADMIN")) {
+            answer = true;
+        }
+        else if(role.equals("ADMIN") && organization.getName().equals("")) {
+            answer = true;
+        }
+        else if(role.equals("ADMIN") && organization.compare(target.getOrganization())) {
+            answer = true;
+        }
+
+        return answer;
     }
 
-    void setEvents(ArrayList<String> events) {
-        this.events = events;
+    /**
+     * Determines if this user can promote the target user to administrator.
+     *
+     * @param target    The target user which this user want to do at action on
+     * @return          True if this user can promote target user to administrator otherwise false
+     */
+    boolean canPromoteToAdmin(User target) {
+        boolean answer = false;
+
+        if(target.getRole().equals("SUPER_ADMIN")) {
+            answer = false;
+        }
+        else if(role.equals("ADMIN") && organization.compare(target.getOrganization())) {
+            answer = true;
+        }
+        else if(role.equals("ADMIN") && organization.getName().equals("")) {
+            answer = true;
+        }
+
+        return answer;
     }
 
-    void setOrganization(Organization organization) {
-        this.organization = organization;
+    /**
+     * Determines if this user can promote the target user to super administrator.
+     *
+     * @param target    The target user which this user want to do at action on
+     * @return          True if this user can promote the target user to super administrator.
+     *                  Otherwise false.
+     */
+    boolean canPromoteToSuperAdmin(User target) {
+        boolean answer = false;
+
+        if(!target.getRole().equals("SUPER_ADMIN") && role.equals("SUPER_ADMIN")) {
+            answer = true;
+        }
+
+        return answer;
     }
 
+    /**
+     * Determines if this user can demote the target user.
+     *
+     * @param target    The target user which this user want to do at action on
+     * @return          True if this user can demote the target user. Otherwise false.
+     */
+    boolean canDemote(User target) {
+        boolean answer = false;
+
+        if(!target.getRole().equals("SUPER_ADMIN")) {
+            if(role.equals("SUPER_ADMIN")) {
+                if(target.getRole().equals("ADMIN") || target.getRole().equals("USER")) {
+                    answer = true;
+                }
+            }
+            else if(role.equals("ADMIN") && target.getRole().equals("USER")) {
+                answer = true;
+            }
+        }
+
+        return answer;
+    }
 }
