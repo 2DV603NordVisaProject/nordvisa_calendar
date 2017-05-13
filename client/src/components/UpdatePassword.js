526a36de0a99ebc9bfcfefdc6dc82af8a6514a78
johang2012 <johan.gudmundsson2012@gmail.com>
2017-05-13 17:48:36 +0200

    init states for UpdatePassword Component

--- client/src/components/UpdatePassword.js
+++ client/src/components/UpdatePassword.js
@@ -1,17 +1,73 @@
 import React, { Component } from "react";
+import ErrorList from "./ErrorList";
 
 class UpdatePassword extends Component {
+  state = {
+    fields: {
+      oldpassword: "",
+      newpassword: "",
+      confirmpassword: "",
+    },
+    fieldErrors: [],
+  }
+
+  validate(fields) {
+    const errors = [];
+    if (!fields.newpassword || !fields.confirmpassword || !fields.oldpassword) errors.push("Password Required!");
+    if (fields.newpassword !== fields.confirmpassword) errors.push("Password's doesn't match!");
+    return errors;
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    const fieldErrors = this.validate(this.state.fields);
+    this.setState({ fieldErrors });
+
+    // Return on Errors
+    if (fieldErrors.length) return;
+
+    this.setState({fields: {
+      oldpassword: "",
+      newpassword: "",
+      confirmpassword: "",
+    }});
+  }
+
+  onInputChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    const fields = this.state.fields;
+    fields[name] = value;
+    this.setState({ fields });
+  }
+
   render() {
     return (
       <div className="box">
         <h3>Update Account Password</h3>
-        <form>
+        <form onSubmit={this.onFormSubmit.bind(this)}>
           <label htmlFor="oldpassword">Old Password:</label>
-          <input type="password" name="oldpassword"></input>
+          <input
+            type="password"
+            name="oldpassword"
+            value={this.state.fields.oldpassword}
+            onChange={this.onInputChange.bind(this)}>
+          </input>
           <label htmlFor="newpassword">New Password:</label>
-          <input type="password" name="newpassword"></input>
+          <input
+            type="password"
+            name="newpassword"
+            value={this.state.fields.newpassword}
+            onChange={this.onInputChange.bind(this)}>
+          </input>
           <label htmlFor="confirmpassword">New Password Again:</label>
-          <input type="password" name="confirmpassword"></input>
+          <input
+            type="password"
+            name="confirmpassword"
+            value={this.state.fields.confirmpassword}
+            onChange={this.onInputChange.bind(this)}>
+          </input>
+          <ErrorList errors={this.state.fieldErrors}/>
           <input type="submit" value="save" className="btn-primary"></input>
         </form>
       </div>
