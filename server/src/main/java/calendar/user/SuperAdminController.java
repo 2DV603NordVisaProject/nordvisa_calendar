c85fa0dc26c65667f785e8d92ec8e88034eeb2da
Axel Nilsson <an222yp@student.lnu.se>
2017-05-13 21:45:32 +0200

    Updated SuperAdminController and added comments

--- server/src/main/java/calendar/user/SuperAdminController.java
+++ server/src/main/java/calendar/user/SuperAdminController.java
@@ -10,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Class SuperAdminController
  *
+ * This class maps to /api/super_admin, and contains available routes for calling the API as an
+ * authenticated user.
+ *
+ * The following routes are available:
+ * Make Super Administrator     -   POST    -   /api/super_admin/make_super_admin
+ *
  * @author Axel Nilsson (axnion)
  */
 @RestController
@@ -17,14 +23,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class SuperAdminController {
     private UserDAO dao = new UserDAOMongo();
 
+    /**
+     * Runs on POST call to /api/super_admin/make_super_admin. Takes an ID though a UserIdDTO, and
+     * if the actor user and target user fulfills some requirements then the role of the target user
+     * is changed
+     *
+     * @param dto           UserIdDTO containing the user ID of the target user
+     * @throws Exception    Database errors
+     */
     // TODO: NOT TESTED!
     @RequestMapping(value = "/make_super_admin", method = RequestMethod.POST)
     public void makeSuperAdministrator(@ModelAttribute UserIdDTO dto) throws Exception {
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        User actor = dao.getUserByEmail("test@test.com"); //TODO: Change argument to email
+        User actor = dao.getUserByEmail(email);
         User target = dao.getUserById(dto.getId());
 
-        if(actor.getRole().equals("SUPER_ADMIN") && !target.getRole().equals("SUPER_ADMIN")) {
+        if(actor.canPromoteToSuperAdmin(target)) {
             dao.setRole(dto.getId(), "SUPER_ADMIN");
         }
     }
