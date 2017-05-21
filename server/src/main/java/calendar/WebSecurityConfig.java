094cfcc260e9b78e5dd0cc757f649d6cd8f84ff2
Axel Nilsson <an222yp@student.lnu.se>
2017-05-21 20:05:09 +0200

    Added custom success and failure handlers on websecurity

--- server/src/main/java/calendar/WebSecurityConfig.java
+++ server/src/main/java/calendar/WebSecurityConfig.java
@@ -10,11 +10,20 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Class WebSecurityConfig
@@ -41,6 +50,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .loginPage("/")
                     .loginProcessingUrl("/login")
                     .permitAll()
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request,
+                                                        HttpServletResponse response,
+                                                        Authentication authentication)
+                            throws IOException, ServletException {
+                        response.setStatus(200);
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        response.sendError(500);
+                    }
+                })
                     .and()
                 .logout()
                     .permitAll()
