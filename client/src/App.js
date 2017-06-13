e7e8a86f286ab7c7188a51b5c6671162f34766bb
Francis <francis.menkes@gmail.com>
2017-06-13 17:10:01 -0400

    Cleans up app.js and client.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,42 @@
 import React, { Component } from 'react';
-import TopBar from "./components/TopBar";
-import ViewContainer from "./components/ViewContainer"
-import Router from "react-router/BrowserRouter";
-import sv from "./i18n/sv";
-import en from "./i18n/en";
-import no from "./i18n/no";
-import da from "./i18n/da";
-import is from "./i18n/is";
-import PropTypes from "prop-types";
-
+import PropTypes from 'prop-types';
+import Router from 'react-router/BrowserRouter';
+import TopBar from './components/TopBar';
+import ViewContainer from './components/ViewContainer';
+import sv from './i18n/sv';
+import en from './i18n/en';
+import no from './i18n/no';
+import da from './i18n/da';
+import is from './i18n/is';
 
 class App extends Component {
-  getChildContext() {
-    return {
-      language: this.state.languages[this.state.currentLanguage]
-    }
+  constructor() {
+    super();
+    this.onLanguageChange = this.onLanguageChange.bind(this);
   }
 
   state = {
-    languages: {
-      sv: sv,
-      en: en,
-      da: da,
-      is: is,
-      no: no,
-    },
-    currentLanguage: "en",
+    languages: { sv, en, da, is, no },
+    currentLanguage: 'en',
+  }
+
+  getChildContext() {
+    return {
+      language: this.state.languages[this.state.currentLanguage],
+    };
   }
 
   onLanguageChange(event) {
     const currentLanguage = event.target.value;
-    this.setState({currentLanguage});
+    this.setState({ currentLanguage });
   }
 
   render() {
     return (
       <Router>
         <div className="App">
-          <TopBar onLanguageChange={this.onLanguageChange.bind(this)}/>
-          <ViewContainer/>
+          <TopBar onLanguageChange={this.onLanguageChange} />
+          <ViewContainer />
         </div>
       </Router>
     );
--- client/src/Client.js
+++ client/src/Client.js
@@ -2,293 +2,273 @@
 
 const url = `${location.protocol}//${location.host}`;
 
-const serialize = function (obj) {
-  var str = [];
-  for(var p in obj)
-    if (obj.hasOwnProperty(p)) {
-      str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+// Serializes an object into foo=bar&baz=quuz form
+const serialize = function serialize(obj) {
+  const str = [];
+  const keys = Object.keys(obj);
+  let i;
+  for (i = 0; i < keys.length; i += 1) {
+    if (Object.prototype.hasOwnProperty.call(obj, keys[i])) {
+      str.push(`${encodeURIComponent(keys[i])}=${encodeURIComponent(obj[keys[i]])}`);
     }
+  }
   return str.join('&');
 };
 
-const getCookie = function(name) {
-  let nameEQ = name + '=';
-  let ca = document.cookie.split(';');
+const getCookie = function getCookie(name) {
+  // const nameEQ = `${name}=`;
+  const ca = document.cookie.split(';');
   let value = null;
-  for(var i=0;i < ca.length;i++) {
-      var currentCookie = ca[i];
-      if (currentCookie.indexOf(name)) {
-        let length = currentCookie.length
-        let valueStart = currentCookie.indexOf('=') + 1;
-        value = currentCookie.substr(valueStart, length);
-      }
-
+  let i;
+  for (i = 0; i < ca.length; i += 1) {
+    const currentCookie = ca[i];
+    if (currentCookie.indexOf(name)) {
+      const length = currentCookie.length;
+      const valueStart = currentCookie.indexOf('=') + 1;
+      value = currentCookie.substr(valueStart, length);
+    }
   }
   return value;
-}
+};
 
 const Client = {
   post: (obj, uri) => {
     const req = new Request(`${url}${uri}`, {
       method: 'POST',
       credentials: 'include',
-      headers: new Headers({ 'Content-Type' : 'application/x-www-form-urlencoded; charset=utf-8' }),
-      body: serialize(obj)
-    })
+      headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' }),
+      body: serialize(obj),
+    });
 
     return fetch(req)
-      .then((res) => {
-        const contentType = res.headers.get('content-type');
+    .then((res) => {
+      const contentType = res.headers.get('content-type');
 
-        if (contentType && contentType.indexOf('application/json') !== -1) {
-          return res.json();
-        }
+      if (contentType && contentType.indexOf('application/json') !== -1) {
+        return res.json();
+      }
 
-        return '';
-      })
-      .then((json) => {
-        return json;
-      })
-      .catch(err => {
-        return err;
-      });
- },
- get: (uri) => {
-   const req = new Request(`${url}${uri}`, {
-     method: 'GET',
-     credentials: 'include',
-     mode: 'no-cors',
-     headers: new Headers({'Content-Type' : 'application/x-www-form-urlencoded; charset=utf-8'}),
-   })
+      return '';
+    })
+    .then(json => json)
+    .catch(err => err);
+  },
+  get: (uri) => {
+    const req = new Request(`${url}${uri}`, {
+      method: 'GET',
+      credentials: 'include',
+      mode: 'no-cors',
+      headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' }),
+    });
 
-   return fetch(req)
-     .then(res => {
-       const contentType = res.headers.get('content-type');
+    return fetch(req)
+    .then((res) => {
+      const contentType = res.headers.get('content-type');
 
-       if (contentType && contentType.indexOf('application/json') !== -1) {
-         return res.json();
-       }
+      if (contentType && contentType.indexOf('application/json') !== -1) {
+        return res.json();
+      }
 
-       return '';
-     })
-     .then(json => {
-       return json;
-     })
-     .catch(err => {
-       return err;
-     });
- },
- postEvent: (eventObj, uri) => {
-   let data = new FormData()
-   data.append('file', eventObj.file)
+      return '';
+    })
+    .then(json => json)
+    .catch(err => err);
+  },
+  postEvent: (eventObj/* , uri*/) => {
+    const data = new FormData();
+    data.append('file', eventObj.file);
 
-   fetch(`${url}/api/upload`, {
-     method: 'POST',
-     body: data
-   }).then(res => {
-     return res.json();
-   }).then(json => {
-     console.log(json);
-   })
- },
- uploadImage: (imageFile) => {
-   let data = new FormData();
-   data.append('files', imageFile);
-   console.log(imageFile);
-   console.log(Array.from(data.entries()));
+    fetch(`${url}/api/upload`, {
+      method: 'POST',
+      body: data,
+    })
+    .then(res => res.json())
+    .then((json) => {
+      console.log(json);
+    });
+  },
+  uploadImage: (imageFile) => {
+    const data = new FormData();
+    data.append('files', imageFile);
 
-   const req = new Request(`${url}/api/upload`, {
-     method: 'POST',
-     body: data
-   });
+    const req = new Request(`${url}/api/upload`, {
+      method: 'POST',
+      body: data,
+    });
 
-   return fetch(req)
-    .then(res => {
-      if(!res.ok) {
+    return fetch(req)
+    .then((res) => {
+      if (!res.ok) {
         throw new Error(res.status);
       }
 
       return res.json();
-    }).then(json => {
-      return json;
-    }).catch(err => {
+    })
+    .then(json => json)
+    .catch((err) => {
       console.log(err.message);
       return err;
     });
- },
- login: function(userObj, uri) {
+  },
+  login: (userObj, uri) => {
+    const req = new Request(`${url}${uri}`, {
+      method: 'POST',
+      mode: 'no-cors',
+      credentials: 'include',
+      headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' }),
+      body: serialize(userObj),
+    });
 
-   const req = new Request(`${url}${uri}`, {
-     method: 'POST',
-     mode: 'no-cors',
-     credentials: 'include',
-     headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' }),
-     body: serialize(userObj)
-   })
+    return fetch(req)
+    .then((res) => {
+      if (res.ok) {
+        document.cookie = 'ESESSION=TRUE; expires=;';
 
-   return fetch(req)
-     .then(res => {
+        const reqUser = new Request(`${url}/api/user/current`, {
+          method: 'GET',
+          mode: 'no-cors',
+          credentials: 'include',
+          headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' }),
+        });
 
-       if (res.ok) {
-         document.cookie = 'ESESSION=TRUE; expires=;';
+        fetch(reqUser)
+        .then(resp => resp.json())
+        .then((json) => {
+          document.cookie = `accessLevel=${json.role}; expires=;`;
+        });
 
-         const reqUser = new Request(`${url}/api/user/current`, {
-           method: 'GET',
-           mode: 'no-cors',
-           credentials: 'include',
-           headers: new Headers({'Content-Type' : 'application/x-www-form-urlencoded; charset=utf-8'}),
-         })
+        return 'success';
+      }
+      return res.json();
+    })
+    .then(json => json)
+    .catch(err => err);
+  },
+  logout: () => {
+    const req = new Request(`${url}/logout`, {
+      method: 'POST',
+      mode: 'no-cors',
+      credentials: 'include',
+      headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' }),
+    });
 
-         fetch(reqUser)
-          .then(res => {
-            return res.json();
-          })
-          .then(json => {
-            document.cookie = `accessLevel=${json.role}; expires=;`
-          })
+    fetch(req)
+    .then((res) => {
+      if (res.ok) {
+        document.cookie = 'ESESSION=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        document.cookie = 'accessLevel=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+      }
+    })
+    .catch(err => err);
+  },
+  isLogedIn: () => {
+    const cookies = document.cookie;
+    if (cookies.indexOf('ESESSION') === -1) {
+      return false;
+    }
+    return true;
+  },
+  getUserLevel: () => {
+    // TODO: accessLevel is used before it's defined. Will always return undefined?
+    if (getCookie(accessLevel) === null) {
+      return;
+    }
 
-         return 'success';
-       } else {
-         return res.json();
-       }
-     })
-     .then(json => {
-       return json;
-     })
-     .catch(err => {
-       return err;
-     });
- },
- logout: () => {
-   const req = new Request(`${url}/logout`, {
-     method: 'POST',
-     mode: 'no-cors',
-     credentials: 'include',
-     headers: new Headers({'Content-Type' : 'application/x-www-form-urlencoded; charset=utf-8'}),
-   })
+    const userLevel = getCookie(accessLevel);
 
-   fetch(req)
-     .then(res => {
-       if (res.ok) {
-         document.cookie = 'ESESSION=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-         document.cookie = 'accessLevel=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-       }
-     })
-     .catch(err => {
-       return err;
-     });
- },
- isLogedIn: () => {
-   const cookies = document.cookie;
-   if (cookies.indexOf('ESESSION') === -1) {
-     return false;
-   } else {
-     return true;
-   }
- },
- getUserLevel: () => {
-   if (getCookie(accessLevel) === null) {
-     return;
-   }
+    let accessLevel = 0;
+    if (userLevel === 'ADMIN') {
+      accessLevel = 1;
+    } else if (userLevel === 'SUPER_ADMIN') {
+      accessLevel = 2;
+    }
+    return accessLevel;
+  },
+  getMembers: () =>
+    [
+      {
+        email: 'sample1@gmail.com',
+        userLevel: 'user',
+      },
+      {
+        email: 'sample2@gmail.com',
+        userLevel: 'admin',
+      },
+      {
+        email: 'sample3@gmail.com',
+        userLevel: 'superadmin',
+      },
+    ],
+  getRegistrations: () =>
+    [
+      {
+        email: 'johan.gudmundsson2012@gmail.com',
+        org: 'The long sample organization',
+      },
+      {
+        email: 'johan.gudmundsson2012@gmail.com',
+        org: 'sample org',
+      },
+      {
+        email: 'axel@gmail.com',
+        org: 'NordVisa',
+      },
+    ],
+  getEvents: () =>
+    [
+      {
+        name: 'Sample Event One',
+        id: 1,
+        date: '2020 July',
+        location: 'YOLO Land',
+        desc: 'test test test test test test test test test',
+      },
+      {
+        name: 'Sample Event Two',
+        id: 2,
+        name: 'Swag?',
+        date: '2020 July',
+        location: 'YOLO Land',
+        desc: 'test test test test test test test test test',
+      },
+      {
+        name: 'Sample Event Three',
+        id: 3,
+        name: 'Swag?',
+        date: '2020 July',
+        location: 'YOLO Land',
+        desc: 'test test test test test test test test test',
+      },
+    ],
+  getEvent: (id) => {
+    const events = [
+      {
+        name: 'Sample Event One',
+        id: 1,
+        date: '2020 July',
+        location: 'YOLO Land',
+        desc: 'test test test test test test test test test',
+      },
+      {
+        name: 'Sample Event Two',
+        id: 2,
+        name: 'Swag?',
+        date: '2020 July',
+        location: 'YOLO Land',
+        desc: 'test test test test test test test test test',
+      },
+      {
+        name: 'Sample Event Three',
+        id: 3,
+        name: 'Swag?',
+        date: '2020 July',
+        location: 'YOLO Land',
+        desc: 'test test test test test test test test test',
+      },
+    ];
 
-   const userLevel = getCookie(accessLevel);
-
-   let accessLevel = 0;
-   if (userLevel === 'ADMIN') {
-     accessLevel = 1;
-   } else if (userLevel === 'SUPER_ADMIN') {
-     accessLevel = 2;
-   }
-   return accessLevel;
- },
- getMembers: () => {
-   return [
-     {
-       email: 'sample1@gmail.com',
-       userLevel: 'user',
-     },
-     {
-       email: 'sample2@gmail.com',
-       userLevel: 'admin',
-     },
-     {
-       email: 'sample3@gmail.com',
-       userLevel: 'superadmin',
-     }
-   ];
- },
- getRegistrations: () => {
-   return [
-     {
-       email: 'johan.gudmundsson2012@gmail.com',
-       org: 'The long sample organization',
-     },
-     {
-       email: 'johan.gudmundsson2012@gmail.com',
-       org: 'sample org',
-     },
-     {
-       email: 'axel@gmail.com',
-       org: 'NordVisa',
-     }
-   ];
- },
- getEvents: () => {
-   return [
-     {
-       name: 'Sample Event One',
-       id: 1,
-       date: '2020 July',
-       location: 'YOLO Land',
-       desc: 'test test test test test test test test test',
-     },
-     {
-       name: 'Sample Event Two',
-       id: 2,
-       name: 'Swag?',
-       date: '2020 July',
-       location: 'YOLO Land',
-       desc: 'test test test test test test test test test',
-     },
-     {
-       name: 'Sample Event Three',
-       id: 3,
-       name: 'Swag?',
-       date: '2020 July',
-       location: 'YOLO Land',
-       desc: 'test test test test test test test test test',
-     },
-   ];
- },
- getEvent: (id) => {
-   const events = [
-     {
-       name: 'Sample Event One',
-       id: 1,
-       date: '2020 July',
-       location: 'YOLO Land',
-       desc: 'test test test test test test test test test',
-     },
-     {
-       name: 'Sample Event Two',
-       id: 2,
-       name: 'Swag?',
-       date: '2020 July',
-       location: 'YOLO Land',
-       desc: 'test test test test test test test test test',
-     },
-     {
-       name: 'Sample Event Three',
-       id: 3,
-       name: 'Swag?',
-       date: '2020 July',
-       location: 'YOLO Land',
-       desc: 'test test test test test test test test test',
-     },
-   ];
-
-   return event = events.find((event) => event.id == id);
- }
+    return events.find(event => event.id === id);
+  },
 };
 
 export default Client;
