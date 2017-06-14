5b7935df6b8c159d0f812ee0657b004cf5ea2ebb
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-15 01:47:42 +0200

    changed to functional component

--- client/src/components/EventsList.js
+++ client/src/components/EventsList.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Event from './Event';
 import PropTypes from 'prop-types';
 
-class EventsList extends Component {
-  render() {
-    const language = this.context.language.MyEventsView;
+const EventsList = (props) => {
+  const language = this.context.language.MyEventsView;
 
-    return (
-      <div className="event-list">
-        <div className="list-header">
-          <p className="capitalize">{language.events}</p>
-        </div>
-        <ul>
-          {
-            this.props.events.map((event, i) => (
-              <Event key={i} event={event} delete={this.props.delete} />
-            ))
-          }
-        </ul>
+  return (
+    <div className="event-list">
+      <div className="list-header">
+        <p className="capitalize">{language.events}</p>
       </div>
-    );
-  }
-}
+      <ul>
+        {
+          this.props.events.map((event, i) => (
+            <Event key={i} event={event} delete={this.props.delete} />
+          ))
+        }
+      </ul>
+    </div>
+  );
+};
 
 
 EventsList.contextTypes = {
