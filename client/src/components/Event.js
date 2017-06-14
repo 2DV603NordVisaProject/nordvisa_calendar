ea0c53684756d87e80e2cff0a30bd338cf6a2645
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-15 01:36:49 +0200

    added PropTypes

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -2,17 +2,17 @@ import React from 'react';
 import Link from 'react-router/Link';
 import PropTypes from 'prop-types';
 
-const Event = () => {
+const Event = (props) => {
   const language = this.context.language.MyEventsView;
 
   return (
     <li>
       <div className="event-item">
-        <p>{this.props.event.name}</p>
+        <p>{props.event.name}</p>
         <div className="item-action-container">
-          <a className="error clickable capitalize" href="" name={this.props.event.id} onClick={this.props.delete.bind(this)}>{language.delete}</a>
-          <Link to={`/user/event/edit/${this.props.event.id}`} className="success capitalize">{language.edit}</Link>
-          <Link to={`/user/event/view/${this.props.event.id}`} className="capitalize">{language.view}</Link>
+          <a className="error clickable capitalize" href="" name={props.event.id} onClick={props.delete}>{language.delete}</a>
+          <Link to={`/user/event/edit/${props.event.id}`} className="success capitalize">{language.edit}</Link>
+          <Link to={`/user/event/view/${props.event.id}`} className="capitalize">{language.view}</Link>
         </div>
       </div>
     </li>
@@ -22,6 +22,14 @@ const Event = () => {
 
 Event.contextTypes = {
   language: PropTypes.object,
+};
+
+Event.propTypes = {
+  event: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+  }).isRequired,
+  delete: PropTypes.func.isRequired,
 };
 
 export default Event;
