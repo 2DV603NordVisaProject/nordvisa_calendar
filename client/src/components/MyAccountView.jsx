1653d767cff341b3c54da1ca16f8ab730ab8ddaf
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-15 04:03:17 +0200

    Changed to functional component

--- client/src/components/MyAccountView.jsx
+++ client/src/components/MyAccountView.jsx
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import UpdatePassword from './UpdatePassword';
 import UpdateAccount from './UpdateAccount';
 
-class MyAccountView extends Component {
-  render() {
-    const language = this.context.language.MyAccountView;
+const MyAccountView = (context) => {
+  const language = context.language.MyAccountView;
 
-    return (
-      <div className="view">
-        <h2 className="capitalize">{language.myAccount}</h2>
-        <UpdateAccount />
-        <UpdatePassword />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="view">
+      <h2 className="capitalize">{language.myAccount}</h2>
+      <UpdateAccount />
+      <UpdatePassword />
+    </div>
+  );
+};
 
 export default MyAccountView;
 
