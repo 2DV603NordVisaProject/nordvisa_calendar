a6536d41ae87cde6b86e1c135e1972d1869483a4
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-15 05:05:17 +0200

    Changed to functional component

--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -1,33 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Registration extends Component {
+const Registration = (props, context) => {
+  const language = context.language.PendingRegistrationsView;
 
-  render() {
-    const language = this.context.language.PendingRegistrationsView;
-
-    return (
-      <li>
-        <div className="pending-item">
-          <div className="email">
-            <p>{this.props.registration.email}</p>
-          </div>
-          <div className="org">
-            <p>{this.props.registration.organization.name || this.props.registration.organization.changePending}</p>
-          </div>
-          <div className="approve-action">
-            <select onChange={this.props.onInputChange.bind(this)} className="capitalize small-form" name={this.props.registration.id}>
-              <option className="capitalize">{language.action}</option>
-              <option className="capitalize small-form" value="approve">{language.approve}</option>
-              <option className="capitalize small-form" value="deny">{language.deny}</option>
-            </select>
-          </div>
+  return (
+    <li>
+      <div className="pending-item">
+        <div className="email">
+          <p>{props.registration.email}</p>
         </div>
-      </li>
-    );
-  }
-}
-
+        <div className="org">
+          <p>{props.registration.organization.name || props.registration.organization.changePending}</p>
+        </div>
+        <div className="approve-action">
+          <select onChange={props.onInputChange} className="capitalize small-form" name={props.registration.id}>
+            <option className="capitalize">{language.action}</option>
+            <option className="capitalize small-form" value="approve">{language.approve}</option>
+            <option className="capitalize small-form" value="deny">{language.deny}</option>
+          </select>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 Registration.contextTypes = {
   language: PropTypes.object,
