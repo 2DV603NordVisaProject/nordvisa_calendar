0c0b1e3a8186bb1a05ce202b353211a679d37414
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-15 03:36:36 +0200

    Removed this

--- client/src/components/Member.jsx
+++ client/src/components/Member.jsx
@@ -9,14 +9,14 @@ const Member = (props) => {
     <li>
       <div className="member-item">
         <div className="email-cell">
-          <p>{this.props.member.email}</p>
+          <p>{props.member.email}</p>
         </div>
         <div className="access-select">
-          <select className="select-full capitalize" onChange={this.props.onChange.bind(this)} name={this.props.member.id} defaultValue={this.props.member.role}>
-            <option value="USER" className="capitalize" disabled={this.props.member.role === 'SUPER_ADMIN'}>{language.user}</option>
-            <option value="ADMIN" className="capitalize" disabled={this.props.member.role === 'SUPER_ADMIN'}>{language.admin}</option>
+          <select className="select-full capitalize" onChange={props.onChange.bind(this)} name={props.member.id} defaultValue={props.member.role}>
+            <option value="USER" className="capitalize" disabled={props.member.role === 'SUPER_ADMIN'}>{language.user}</option>
+            <option value="ADMIN" className="capitalize" disabled={props.member.role === 'SUPER_ADMIN'}>{language.admin}</option>
             <option value="SUPER_ADMIN" className="capitalize">{language.superadmin}</option>
-            <option value="DELETE" className="capitalize" disabled={this.props.member.role === 'SUPER_ADMIN'}>{language.delete}</option>
+            <option value="DELETE" className="capitalize" disabled={props.member.role === 'SUPER_ADMIN'}>{language.delete}</option>
           </select>
         </div>
       </div>
