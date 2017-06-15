4f2957a377d8dfb0f33365fbc4d3428f2d22a87e
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-15 03:28:16 +0200

    Changed to functional component

--- client/src/components/MapMarker.jsx
+++ client/src/components/MapMarker.jsx
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './MapMarker.css';
 
-class MapMarker extends Component {
-  render() {
-    return (
-      <div>
-        <div className="pin bounce" />
-        <div className="pulse" />
-      </div>
-    );
-  }
-}
+const MapMarker = () => (
+  <div>
+    <div className="pin bounce" />
+    <div className="pulse" />
+  </div>
+);
 
 
 export default MapMarker;
