b208db6b5bb9008e6488c72d81741fabc758f422
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-15 05:12:35 +0200

    Changed to functional component

--- client/src/components/RegistrationsList.jsx
+++ client/src/components/RegistrationsList.jsx
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Registration from './Registration';
 
-class RegistrationsList extends Component {
-  render() {
-    return (
-      <ul>
-        {
-          this.props.registrations.map((registration, i) => (
-            <Registration key={i} registration={registration} onInputChange={this.props.onInputChange} />
-          ))
-        }
-      </ul>
-    );
-  }
-}
+const RegistrationsList = props => (
+  <ul>
+    {
+      props.registrations.map((registration, i) => (
+        <Registration key={i} registration={registration} onInputChange={props.onInputChange} />
+      ))
+    }
+  </ul>
+);
 
 
 export default RegistrationsList;
