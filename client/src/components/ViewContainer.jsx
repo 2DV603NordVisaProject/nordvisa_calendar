b6e8b15eafa28849128017d2f55e1165dcfc1480
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-15 06:29:12 +0200

    Changed to functional component

--- client/src/components/ViewContainer.jsx
+++ client/src/components/ViewContainer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Miss from 'react-router/Miss';
 import Match from 'react-router/Match';
 import Redirect from 'react-router/Redirect';
@@ -19,70 +19,66 @@ import MatchWhenAdmin from './MatchWhenAdmin';
 import Client from '../Client';
 
 
-class ViewContainer extends Component {
-  render() {
-    return (
-      <div className="view-container">
-        <Miss render={({ location }) => (
-          <div className="lightbox error404">
-            <h2>404</h2>
-            <h3>{location.pathname} could not be found!</h3>
-          </div>
-          )}
-        />
-        <Match
-          exactly
-          pattern="/"
-          render={() => (
-            <Redirect to="/login" />
-        )}
-        />
-        <Match pattern="/login" component={LoginView} />
-        <Match pattern="/logout" component={Logout} />
-        <Match pattern="/register" component={RegisterView} />
-        <Match pattern="/recover-password" component={RecoverView} />
-        <Match
-          pattern="/update-password/:id"
-          render={({ params }) => (
-            <UpdatePasswordView id={params.id} />
-            )}
-        />
-        <Match pattern="/generate-widget" component={WidgetView} />
-        <MatchWhenLoggedIn pattern="/user/account" component={MyAccountView} />
-        <MatchWhenLoggedIn pattern="/user/event/create" component={CreateView} />
-        <MatchWhenLoggedIn exactly pattern="/user/event" component={MyEventsView} />
-        <Match
-          pattern="/user/event/edit/:id"
-          render={({ params }) => {
-            if (Client.isLogedIn()) {
-              return (
-                <CreateView id={params.id} progress="edit" />
-              );
-            }
-            return (
-              <Redirect to="/login" />
-            );
-          }}
-        />
-
-        <Match
-          pattern="/user/event/view/:id"
-          render={({ params }) => {
-            if (Client.isLogedIn()) {
-              return (
-                <CreateView id={params.id} progress="view" />
-              );
-            }
-            return (
-              <Redirect to="/login" />
-            );
-          }}
-        />
-        <MatchWhenAdmin pattern="/admin/members" component={MembersView} />
-        <MatchWhenAdmin pattern="/admin/pending-registrations" component={PendingRegistrationsView} />
+const ViewContainer = () => (
+  <div className="view-container">
+    <Miss render={({ location }) => (
+      <div className="lightbox error404">
+        <h2>404</h2>
+        <h3>{location.pathname} could not be found!</h3>
       </div>
-    );
-  }
-}
+      )}
+    />
+    <Match
+      exactly
+      pattern="/"
+      render={() => (
+        <Redirect to="/login" />
+    )}
+    />
+    <Match pattern="/login" component={LoginView} />
+    <Match pattern="/logout" component={Logout} />
+    <Match pattern="/register" component={RegisterView} />
+    <Match pattern="/recover-password" component={RecoverView} />
+    <Match
+      pattern="/update-password/:id"
+      render={({ params }) => (
+        <UpdatePasswordView id={params.id} />
+        )}
+    />
+    <Match pattern="/generate-widget" component={WidgetView} />
+    <MatchWhenLoggedIn pattern="/user/account" component={MyAccountView} />
+    <MatchWhenLoggedIn pattern="/user/event/create" component={CreateView} />
+    <MatchWhenLoggedIn exactly pattern="/user/event" component={MyEventsView} />
+    <Match
+      pattern="/user/event/edit/:id"
+      render={({ params }) => {
+        if (Client.isLogedIn()) {
+          return (
+            <CreateView id={params.id} progress="edit" />
+          );
+        }
+        return (
+          <Redirect to="/login" />
+        );
+      }}
+    />
+
+    <Match
+      pattern="/user/event/view/:id"
+      render={({ params }) => {
+        if (Client.isLogedIn()) {
+          return (
+            <CreateView id={params.id} progress="view" />
+          );
+        }
+        return (
+          <Redirect to="/login" />
+        );
+      }}
+    />
+    <MatchWhenAdmin pattern="/admin/members" component={MembersView} />
+    <MatchWhenAdmin pattern="/admin/pending-registrations" component={PendingRegistrationsView} />
+  </div>
+);
 
 export default ViewContainer;
