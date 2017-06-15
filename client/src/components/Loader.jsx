2a3e24d95ea73ab2697a2d94f784ac7761ab650a
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-15 03:19:39 +0200

    Changed to functional component

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Loader.css';
 
-class Loader extends Component {
-  render() {
-    return (
-      <div className="load-container">
-        <div className="loader" />
-        <p className="load">Loading</p>
-      </div>
-    );
-  }
-}
+const Loader = props => (
+  <div className="load-container">
+    <div className="loader" />
+    <p className="load">Loading</p>
+  </div>
+);
 
 export default Loader;
