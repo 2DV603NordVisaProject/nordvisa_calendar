329289c83f4d4d48c410b7cc49a8958f4cce7a4c
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-15 05:26:54 +0200

    Changed to functional component

--- client/src/components/TopBar.jsx
+++ client/src/components/TopBar.jsx
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Link from 'react-router/Link';
 import PropTypes from 'prop-types';
 import './TopBar.css';
 import ResponsiveMenu from './ResponsiveMenu';
 
-class TopBar extends Component {
-  render() {
-    const language = this.context.language.TopBar;
+const TopBar = (props, context) => {
+  const language = context.language.TopBar;
 
-    return (
-      <div className="topbar">
-        <div className="brand">
-          <Link to="/user/event">Event Dashboard</Link>
-        </div>
-        <ResponsiveMenu />
-        <select defaultValue="en" onChange={this.props.onLanguageChange.bind(this)} className="capitalize">
-          <option value="sv" className="capitalize">{language.swedish}</option>
-          <option value="en" className="capitalize">{language.english}</option>
-          <option value="no" className="capitalize">{language.norwegian}</option>
-          <option value="da" className="capitalize">{language.danish}</option>
-          <option value="is" className="capitalize">{language.icelandic}</option>
-        </select>
+  return (
+    <div className="topbar">
+      <div className="brand">
+        <Link to="/user/event">Event Dashboard</Link>
       </div>
-    );
-  }
-}
+      <ResponsiveMenu />
+      <select defaultValue="en" onChange={props.onLanguageChange} className="capitalize">
+        <option value="sv" className="capitalize">{language.swedish}</option>
+        <option value="en" className="capitalize">{language.english}</option>
+        <option value="no" className="capitalize">{language.norwegian}</option>
+        <option value="da" className="capitalize">{language.danish}</option>
+        <option value="is" className="capitalize">{language.icelandic}</option>
+      </select>
+    </div>
+  );
+};
 
 TopBar.contextTypes = {
   language: PropTypes.object,
