ff1067b5259c832c490b25230cf7072ef4e3a3bb
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-18 23:12:03 +0200

    Added tests for LoginView Component

---
+++ client/tests/LoginView.test.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import LoginView from '../src/components/LoginView';
+import en from '../src/i18n/en';
+import helpers from './helpers';
+
+describe('LoginView', () => {
+  let wrapper;
+  const location = {
+    state: {
+      referrer: '',
+    },
+  };
+  const context = { language: en };
+
+  beforeEach(() => {
+    wrapper = shallow(<LoginView location={location} />, { context });
+  });
+
+  it('should be defined', () => {
+    expect(wrapper).toBeDefined();
+  });
+
+  it('should render', () => {
+    expect(wrapper.find('div').length).toBeGreaterThan(0);
+  });
+
+  it('should contain Link-component', () => {
+    expect(wrapper.find('Link').length).toBe(1);
+  });
+
+  it('should contain ErrorList-component', () => {
+    expect(wrapper.find('ErrorList').length).toBe(1);
+  });
+
+  describe('user enter an email-adress', () => {
+    const value = 'sample@email.com';
+
+    beforeEach(() => {
+      const input = wrapper.find('input').first();
+
+
+      input.simulate('change', {
+        target: {
+          value,
+          name: 'email',
+        },
+      });
+    });
+
+    it("should update state 'field.email'", () => {
+      expect(wrapper.state().fields.email).toBe(value);
+    });
+
+    describe('user clicks login', () => {
+      beforeEach(() => {
+        const btn = wrapper.find('input[type="submit"]').first();
+
+        btn.simulate('click', {
+          preventDefault: () => {},
+        });
+      });
+
+      it('should add error to state fieldErrors', () => {
+        expect(wrapper.state().fieldErrors.length).toBe(1);
+      });
+    });
+
+    describe('user enter a long password', () => {
+      const longPassword = helpers.generateString(300);
+      const input = wrapper.find('input').at(2);
+
+      beforeEach(() => {
+        input.simulate('change', {
+          target: {
+            value: longPassword,
+            name: 'password',
+          },
+        });
+      });
+
+      it('should add error to state fieldErrors', () => {
+        expect(wrapper.state().fieldErrors.length).toBe(1);
+      });
+    });
+
+    describe('user enters a valid password', () => {
+      describe('user entera a invalid email', () => {
+        it('should add error to state fieldErrors', () => {
+          expect(wrapper.state().fieldErrors.length).toBe(1);
+        });
+        describe('user enters a too short password', () => {
+          it('UI should display 2 errors', () => {
+            expect(wrapper.state().fieldErrors.length).toBe(1);
+          });
+          it('should add error to state fieldErrors', () => {
+            expect(wrapper.state().fieldErrors.length).toBe(1);
+          });
+          it("should update state 'field.password'", () => {
+            // Body...
+          });
+        });
+      });
+
+      describe('user clicks login', () => {
+        it('state loginInProgress should be set to true', () => {
+          // Body...
+        });
+
+        describe('login is successfull', () => {
+          it('state shouldRedirect should be set to true', () => {
+            // Body...
+          });
+          it('state fields should be empty', () => {
+            // Body...
+          });
+        });
+        describe('login is not successfull', () => {
+          it('state loginInProgress should be set to false', () => {
+            // Body...
+          });
+          it('should add error to fieldErrors', () => {
+            // Body...
+          });
+        });
+      });
+    });
+  });
+});
