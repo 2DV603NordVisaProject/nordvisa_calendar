3aa7c04ad39e023ef287de34e7a8d9f3c88e02b1
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-20 00:37:34 +0200

    Changed EventsMap component to functional component

--- client/src/components/EventsMap.jsx
+++ client/src/components/EventsMap.jsx
@@ -1,39 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import PropTypes from 'prop-types';
 import MapMarker from './MapMarker';
 
-class EventsMap extends Component {
+const EventsMap = props => (
+  <GoogleMapReact
+    defaultCenter={props.center}
+    defaultZoom={props.zoom}
+    bootstrapURLKeys={{
+      key: 'AIzaSyDIKbno4BEY3C76YjjHOvYfDc1kYEAYd4w',
+      language: 'en',
+    }}
+  >
 
-  static defaultProps = {
-    center: { lat: 59.329323, lng: 18.068581 },
-    zoom: 11,
-  };
-
-  render() {
-    return (
-      <GoogleMapReact
-        defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}
-        bootstrapURLKeys={{
-          key: 'AIzaSyDIKbno4BEY3C76YjjHOvYfDc1kYEAYd4w',
-          language: 'en',
-        }}
-      >
-
-        {
-        this.props.events.map(event => (
+    {
+        props.events.map(event => (
           <MapMarker
             lat={event.location.coordinates.coordinates[1]}
             lng={event.location.coordinates.coordinates[0]}
           />
       ))
       }
-      </GoogleMapReact>
-    );
-  }
+  </GoogleMapReact>
+);
 
-}
+EventsMap.defaultProps = {
+  center: { lat: 59.329323, lng: 18.068581 },
+  zoom: 11,
+};
 
 EventsMap.propTypes = {
   center: PropTypes.shape({
