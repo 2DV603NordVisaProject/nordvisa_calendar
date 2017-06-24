fbd97bccf7211855577e9731a10feceaa5f900c8
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-25 00:08:45 +0200

    Destructed propsparam

--- client/src/components/ConfirmMessage.jsx
+++ client/src/components/ConfirmMessage.jsx
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import './ConfirmMessage.css';
 import Button from './Button';
 
-const ConfirmMessage = props => (
+const ConfirmMessage = ({ popup, onYesClick, onNoClick }) => (
   <div className="confirm-message">
-    <p>{props.popup.msg}</p>
+    <p>{popup.msg}</p>
     <div className="answer">
-      <Button theme="success" onClick={props.onYesClick}>yes</Button>
-      <Button theme="error" onClick={props.onNoClick}>no</Button>
+      <Button theme="success" onClick={onYesClick}>yes</Button>
+      <Button theme="error" onClick={onNoClick}>no</Button>
     </div>
   </div>
   );
