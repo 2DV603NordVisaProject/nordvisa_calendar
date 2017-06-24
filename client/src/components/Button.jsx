b0d087fdd0f77121195626be2c450565bf7b8762
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-25 00:07:39 +0200

    Moved props to top

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Button.css';
 
+const propTypes = {
+  theme: PropTypes.string,
+  children: PropTypes.string,
+  onClick: PropTypes.func,
+  form: PropTypes.boolean,
+};
+
+const defaultProps = {
+  theme: 'primary',
+  children: 'submit',
+  onClick: null,
+  form: false,
+};
+
 const Button = ({ form, theme, children, onClick }) => {
   if (form) {
     return <input className={theme} type="submit" value={children} />;
@@ -9,18 +23,7 @@ const Button = ({ form, theme, children, onClick }) => {
   return <button className={theme} onClick={onClick}>{children}</button>;
 };
 
-Button.defaultProps = {
-  theme: 'primary',
-  children: 'submit',
-  onClick: null,
-  form: false,
-};
-
-Button.propTypes = {
-  theme: PropTypes.string,
-  children: PropTypes.string,
-  onClick: PropTypes.func,
-  form: PropTypes.boolean,
-};
+Button.defaultProps = defaultProps;
+Button.propTypes = propTypes;
 
 export default Button;
