87ba28b959ccb975677ec0a8c2133a954bd2ce88
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-25 00:44:18 +0200

    destructed props param

--- client/src/components/GeneratedOutput.jsx
+++ client/src/components/GeneratedOutput.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GeneratedOutput = (props, context) => {
+const GeneratedOutput = ({ isGenerated, headCode, bodyCode }, context) => {
   const language = context.language.WidgetView;
-  if (props.isGenerated) {
+  if (isGenerated) {
     return (
       <div className="code-container">
         <p>{language.headCode}:</p>
-        <textarea className="widget-code" defaultValue={props.headCode} disabled />
+        <textarea className="widget-code" defaultValue={headCode} disabled />
         <p>{language.bodyCode}:</p>
-        <textarea className="widget-code" defaultValue={props.bodyCode} disabled />
+        <textarea className="widget-code" defaultValue={bodyCode} disabled />
       </div>
     );
   }
