7c2b773e0cf547d9a1aadea3106c33a4aae11da0
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-25 00:46:02 +0200

    moved propTypes, defaultProps and contextTypes to top

--- client/src/components/GeneratedOutput.jsx
+++ client/src/components/GeneratedOutput.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const propTypes = {
+  headCode: PropTypes.string.isRequired,
+  bodyCode: PropTypes.string.isRequired,
+  isGenerated: PropTypes.boolean,
+};
+
+const defaultProps = {
+  isGenerated: false,
+};
+
+const contextTypes = {
+  language: PropTypes.object,
+};
+
 const GeneratedOutput = ({ isGenerated, headCode, bodyCode }, context) => {
   const language = context.language.WidgetView;
   if (isGenerated) {
@@ -16,18 +30,8 @@ const GeneratedOutput = ({ isGenerated, headCode, bodyCode }, context) => {
   return null;
 };
 
-GeneratedOutput.propTypes = {
-  headCode: PropTypes.string.isRequired,
-  bodyCode: PropTypes.string.isRequired,
-  isGenerated: PropTypes.boolean,
-};
-
-GeneratedOutput.defaultProps = {
-  isGenerated: false,
-};
-
-GeneratedOutput.contextTypes = {
-  language: PropTypes.object,
-};
+GeneratedOutput.propTypes = propTypes;
+GeneratedOutput.defaultProps = defaultProps;
+GeneratedOutput.contextTypes = contextTypes;
 
 export default GeneratedOutput;
