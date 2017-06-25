a66225763e7762af49738f925b2a4bbf2032151a
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-25 04:32:34 +0200

    moved propTypes to the top

--- client/src/components/PageTitle.jsx
+++ client/src/components/PageTitle.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './PageTitle.css';
 
+const propTypes = {
+  children: PropTypes.string.isRequired,
+};
+
 const PageTitle = ({ children }) => (
   <h2 className="page-title">{children}</h2>
 );
 
-PageTitle.propTypes = {
-  children: PropTypes.string.isRequired,
-};
+PageTitle.propTypes = propTypes;
 
 export default PageTitle;
