db0493dbb4c4a5342fa8189c813327faab90641c
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-25 05:08:30 +0200

    moved propTypes and contextTypes to the top

--- client/src/components/SubTitle.jsx
+++ client/src/components/SubTitle.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './SubTitle.css';
 
+const propTypes = {
+  children: PropTypes.string,
+};
+
+const defaultProps = {
+  children: 'Sub Title',
+};
+
 const SubTitle = ({ children }) => (
   <h3 className="sub-title">{children}</h3>
 );
 
-SubTitle.defaultProps = {
-  children: 'Sub Title',
-};
-
-SubTitle.propTypes = {
-  children: PropTypes.string,
-};
+SubTitle.propTypes = propTypes;
+SubTitle.defaultProps = defaultProps;
 
 export default SubTitle;
