bcde4c7cc776f9292c1eab88aa7360f243615987
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-25 05:29:42 +0200

    moved propTypes and defaultProps to the top

--- client/src/components/ViewContainer.jsx
+++ client/src/components/ViewContainer.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ViewContainer.css';
 
+const propTypes = {
+  children: PropTypes.element.isRequired,
+  size: PropTypes.string,
+};
+
+const defaultProps = {
+  size: 'large',
+};
+
 const ViewContainer = props => (
   <div className={`${props.size} ${props.className}`}>{props.children}</div>
 );
 
-ViewContainer.defaultProps = {
-  size: 'large',
-};
-
-ViewContainer.propTypes = {
-  children: PropTypes.element.isRequired,
-  size: PropTypes.string,
-};
+ViewContainer.propTypes = propTypes;
+ViewContainer.defaultProps = defaultProps;
 
 export default ViewContainer;
