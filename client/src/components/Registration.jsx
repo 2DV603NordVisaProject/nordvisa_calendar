46520a3f5c90d552ad2e4184e094918a46c7af86
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-25 04:58:29 +0200

    destructuring props param

--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Registration = (props, context) => (
+const Registration = ({ registration, onInputChange }, context) => (
   <li>
     <div className="pending-item">
       <div className="email">
-        <p>{props.registration.email}</p>
+        <p>{registration.email}</p>
       </div>
       <div className="org">
         <p>
           {
-            props.registration.organization.name || props.registration.organization.changePending
+            registration.organization.name || registration.organization.changePending
           }
         </p>
       </div>
       <div className="approve-action">
-        <select onChange={props.onInputChange} className="small-form" style={{ textTransform: 'capitalize' }} name={props.registration.id}>
+        <select onChange={onInputChange} className="small-form" style={{ textTransform: 'capitalize' }} name={registration.id}>
           <option>{context.language.PendingRegistrationsView.action}</option>
           <option className="small-form" value="approve">{context.language.PendingRegistrationsView.approve}</option>
           <option className="small-form" value="deny">{context.language.PendingRegistrationsView.deny}</option>
