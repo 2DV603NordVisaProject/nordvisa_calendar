c52a454da44ae958754ba108e19210a31785ea72
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-25 02:44:23 +0200

    destructing props param

--- client/src/components/Member.jsx
+++ client/src/components/Member.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-const Member = (props, context) => (
+const Member = ({ member, onChange }, context) => (
   <li>
     <div className="member-item">
       <div className="email-cell">
-        <p>{props.member.email}</p>
+        <p>{member.email}</p>
       </div>
       <div className="access-select">
-        <select className="select-full" style={{ textTransform: 'capitalize' }} onChange={props.onChange} name={props.member.id} defaultValue={props.member.role}>
-          <option value="USER" className="capitalize" disabled={props.member.role === 'SUPER_ADMIN'}>{context.language.MembersView.user}</option>
-          <option value="ADMIN" className="capitalize" disabled={props.member.role === 'SUPER_ADMIN'}>{context.language.MembersView.admin}</option>
+        <select className="select-full" style={{ textTransform: 'capitalize' }} onChange={onChange} name={member.id} defaultValue={member.role}>
+          <option value="USER" className="capitalize" disabled={member.role === 'SUPER_ADMIN'}>{context.language.MembersView.user}</option>
+          <option value="ADMIN" className="capitalize" disabled={member.role === 'SUPER_ADMIN'}>{context.language.MembersView.admin}</option>
           <option value="SUPER_ADMIN" className="capitalize">{context.language.MembersView.superadmin}</option>
-          <option value="DELETE" className="capitalize" disabled={props.member.role === 'SUPER_ADMIN'}>{context.language.MembersView.delete}</option>
+          <option value="DELETE" className="capitalize" disabled={member.role === 'SUPER_ADMIN'}>{context.language.MembersView.delete}</option>
         </select>
       </div>
     </div>
