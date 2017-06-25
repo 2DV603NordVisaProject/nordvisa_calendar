6f4c16d1a6d7a2d7549c4808f5b802ad4471794f
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-25 13:10:45 +0200

    added propTypes

--- client/src/components/MatchWhenAdmin.jsx
+++ client/src/components/MatchWhenAdmin.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import Match from 'react-router/Match';
 import Redirect from 'react-router/Redirect';
+import PropTypes from 'prop-types';
 import Client from '../Client';
+
+const propTypes = {
+  component: PropTypes.element.isRequired,
+};
 
 const MatchWhenAdmin = ({ component: Component, ...rest }) => (
   <Match
@@ -26,5 +31,6 @@ const MatchWhenAdmin = ({ component: Component, ...rest }) => (
   />
 );
 
+MatchWhenAdmin.propTypes = propTypes;
 
 export default MatchWhenAdmin;
