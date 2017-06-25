259ec8adc8d938f239d775c9066d9d74f318d66b
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-25 02:54:27 +0200

    moved propTypes and contextTypes to the top

--- client/src/components/MembersList.jsx
+++ client/src/components/MembersList.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Member from './Member';
 
+const propTypes = {
+  onChange: PropTypes.func.isRequired,
+  members: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
+
+const contextTypes = {
+  language: PropTypes.object,
+};
+
 const MembersList = ({ members, onChange }, context) => (
   <div className="members-list">
     <div className="list-header">
@@ -18,15 +29,7 @@ const MembersList = ({ members, onChange }, context) => (
   </div>
 );
 
-MembersList.contextTypes = {
-  language: PropTypes.object,
-};
-
-MembersList.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  members: PropTypes.arrayOf(
-    PropTypes.object,
-  ).isRequired,
-};
+MembersList.propTypes = propTypes;
+MembersList.contextTypes = contextTypes;
 
 export default MembersList;
