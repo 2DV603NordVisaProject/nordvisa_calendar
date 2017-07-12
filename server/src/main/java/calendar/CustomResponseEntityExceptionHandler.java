06c91b498f345f87bcb7dc2aedce06147b1550a5
axnion <an222yp@student.lnu.se>
2017-07-12 07:39:38 +0200

    Added lines to .gitignore to work with vscode

--- server/src/main/java/calendar/CustomResponseEntityExceptionHandler.java
+++ server/src/main/java/calendar/CustomResponseEntityExceptionHandler.java
@@ -1,22 +1,13 @@
 package calendar;
 
 import calendar.image.dto.UploadImagesDTO;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.google.gson.JsonObject;
-import com.mongodb.util.JSON;
 import org.apache.tomcat.util.http.fileupload.FileUploadBase;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
